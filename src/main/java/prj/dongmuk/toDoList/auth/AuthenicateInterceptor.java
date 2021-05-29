2aede33799b37ba467544f8c1b8dec65760f361c
dodomuk <siderid@Naver.com>
2021-05-29 18:16:01 +0900

    [210529동묵] todolist 부트 0.1

--- src/main/java/prj/dongmuk/toDoList/auth/AuthenicateInterceptor.java
+++ src/main/java/prj/dongmuk/toDoList/auth/AuthenicateInterceptor.java
@@ -7,11 +7,12 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
+//로그인 확인을 위한 인터셉터
 @Log4j2
 public class AuthenicateInterceptor implements HandlerInterceptor {
 
     @Override
-    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)throws IOException{
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler){
         if(request.getSession().getAttribute("user")==null){
             return false;
         }
