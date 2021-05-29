fbb1aff6711453bd4bb03db2363efe019fad3e85
dodomuk <siderid@Naver.com>
2021-05-29 18:31:26 +0900

    [210529동묵] todolist 부트 0.1

--- src/main/java/prj/dongmuk/toDoList/config/MvcConfiguration.java
+++ src/main/java/prj/dongmuk/toDoList/config/MvcConfiguration.java
@@ -5,6 +5,8 @@ import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistra
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+//개발자가 직접 제어가 불가능한 외부 라이브러리 또는 설정을 위한 클래스를 bean 등록할 때 사용
+//번외) Component: 개발자가 직접 개발한 클래스를 bean 등록할때 활용
 @Configuration
 public class MvcConfiguration implements WebMvcConfigurer {
 
